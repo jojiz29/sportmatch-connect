edc4d2fc4d9cd1664565284bf7823ebcf281e7c6
jojiz29 <salvatierralonso1819@gmail.com>
2026-06-08 02:39:17 -0500

    fix: importar AuthModule en todos los módulos que usan SupabaseAuthGuard

--- server/src/courts/courts.module.ts
+++ server/src/courts/courts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CourtsController } from './courts.controller';
 import { CourtsService } from './courts.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [CourtsController],
   providers: [CourtsService],
   exports: [CourtsService],
